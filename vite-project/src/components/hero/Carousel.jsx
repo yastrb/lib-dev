ad5c1d5a0de443db8e2dbd600b202b0f1b0b31fb
Olena Yastrebtsova <uaalena202@gmail.com>
2024-05-15 14:33:23 +0300

    added: slider

--- vite-project/src/components/hero/Carousel.jsx
+++ vite-project/src/components/hero/Carousel.jsx
@@ -15,37 +15,41 @@ const Carousel = () => {
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: true,
+          dots: true
         }
       },
       {
-        breakpoint: 360,
+        breakpoint: 769,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1,
-          infinite: true,
-          dots: true,
+          initialSlide: 2
         }
       },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
     ]
   };
 
   
   return (
-    <div className="slider-container ">
+    <div className="slider-container md:px-0 px-5 ">
       <Slider {...settings}>
         {data.map((item) => {
           const { id, img } = item;
           return (
-            <div className='' key={id}>
-              <div className='mx-3'>
+            <div key={id}>
                 <img className=' mx-auto' src={img} alt="img" />
-              </div>
             </div>
           )
         }
