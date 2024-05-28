788d9ab10450be6d830151a8fef83a120232608b
Olena Yastrebtsova <78073513+yastrb@users.noreply.github.com>
2024-05-28 15:01:18 +0300

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,25 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-git-dev-bibliotekas-projects.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
+})
+
 
