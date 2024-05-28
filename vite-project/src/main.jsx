d4cb4b6838e64e275b259f375dffb64345031923
Olena Yastrebtsova <78073513+yastrb@users.noreply.github.com>
2024-05-28 14:58:01 +0300

    Update main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from 'react-query';
+import queryClient from './queryClient';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
