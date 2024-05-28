6acad49d805f8e2d009ea70ee55ddf65c38c16ff
Olena Yastrebtsova <uaalena202@gmail.com>
2024-05-28 15:00:13 +0300

    delete: react query

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClientProvider } from 'react-query';
-import queryClient from './queryClient'; // імпортуємо створений QueryClient
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </React.StrictMode>
 );
 
