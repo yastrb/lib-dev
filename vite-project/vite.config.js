12da70d77029a5553ddaef89de753aaf18c7cdba
Olena Yastrebtsova <uaalena202@gmail.com>
2024-05-29 13:09:25 +0300

    Committing local changes to vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios'],
+      external: ['axios'], // Зовнішній модуль
     },
   },
   server: {
@@ -18,6 +18,7 @@ export default defineConfig({
     },
   },
 })
+
 
 
 
