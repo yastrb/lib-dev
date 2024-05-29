833b442c559f16ad64220598d2a0b93525284a03
Olena Yastrebtsova <uaalena202@gmail.com>
2024-05-29 13:05:47 +0300

    update: vite.config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
   server: {
     proxy: {
       '/api': {
