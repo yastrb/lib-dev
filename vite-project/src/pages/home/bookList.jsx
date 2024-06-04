63482dcfefe0a88c37c7151a174e0a26be824774
Olena Yastrebtsova <uaalena202@gmail.com>
2024-06-04 12:37:09 +0300

    Налаштування змінних оточення

--- vite-project/src/pages/home/bookList.jsx
+++ vite-project/src/pages/home/bookList.jsx
@@ -5,31 +5,10 @@ const BookList = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchBooks = async () => {
-  //     try {
-  //       const response = await axios.get('/api/');
-  //       setBooks(response.data.newBooks);
-  //       setLoading(false);
-  //     } catch (err) {
-  //       setError(err);
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchBooks();
-  // }, []);
-
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error fetching books</div>;
-
-  // console.log(books);
-
-  // return null;
-
+  
   useEffect(()=> {
-    axios.get('/api/')
+    const apiUrl = `${import.meta.env.VITE_API_URL}/api/`;
+    axios.get(apiUrl)
     .then(response => {
       setBooks(response.data.newBooks);
       setLoading(false);
@@ -41,7 +20,7 @@ const BookList = () => {
   },[]);
   
   if (loading) return <div>loading</div> ;
-  if(error) return <div> error</div>;
+  if(error) return <div> Error</div>;
 
   return (
     <div className='my-4'>
