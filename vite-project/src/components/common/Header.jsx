13a0b8429a6ebe2da258bf9fed2e9131a9048ab6
Olena Yastrebtsova <uaalena202@gmail.com>
2024-06-05 13:47:57 +0300

    fixed: search function

--- vite-project/src/components/common/Header.jsx
+++ vite-project/src/components/common/Header.jsx
@@ -7,12 +7,44 @@ import SearchBar from './SearchBar';
 import SelectLanguage from './SelectLanguage';
 import Nav from './Nav';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 
 const Header = () => {
 
- 
+  const [books, setBooks] = useState([]);
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('/api/')
+      .then(({ data }) => {
+        const { newBooks, salesBooks, bestsellerBooks } = data;
+        const combinedBooks = [...newBooks, ...salesBooks, ...bestsellerBooks];
+        // setBooks(combinedBooks);
+        setFilteredBooks(combinedBooks);
+        setLoading(false);
+        console.log(combinedBooks)
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleFilter = (event) => {
+    const res = filteredBooks.filter(b => b && b.title ? b.title.toLowerCase().includes(event.target.value) : false);
+    setBooks(res);
+  };
+
+  if (loading) return <div>Завантаження книг...</div>;
+  if (error) return <div>Помилка при завантаженні книг: {error.message}</div>;
+
+
   return (
     <header className=' w-full '>
       <div className='bg-main pt-[44px]'>
@@ -22,8 +54,15 @@ const Header = () => {
             <img src={logo} alt="BookShop" className=' w-[180px] h-[80px]' />
           </Link>
 
-          <div className=' hidden md:flex md:items-center md:justify-center'>
-            <SearchBar  />
+          <div className=' hidden md:flex md:flex-col md:items-center md:justify-center relative'>
+            <SearchBar onChange={handleFilter} />
+            
+              <ul className=' px-2 absolute top-20 z-50 rounded-md shadow bg-white w-full'>
+                {books.map(item => (
+                  <li className='my-2' key={item.id}>{item.title}</li>
+                ))}
+              </ul>
+        
           </div>
 
           <div className=' flex md:items-center md:justify-center'>
