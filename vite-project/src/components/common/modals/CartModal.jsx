1f8dc32ab6dc1381ccd325bcf35fd7f720b235ea
Olena Yastrebtsova <uaalena202@gmail.com>
2024-06-11 20:00:20 +0300

    update CartModal

--- vite-project/src/components/common/modals/CartModal.jsx
+++ vite-project/src/components/common/modals/CartModal.jsx
@@ -1,38 +1,43 @@
 import close from '../../../assets/close-modal.svg';
 import { removeFromCart } from '../../../redux/cartSlice';
 import styles from '../../../style';
-import { useSelector, useDispatch} from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
 
 const CartModal = ({ toggleModal }) => {
     const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.cart.cartItems);
     const amount = useSelector((state) => state.cart.amount);
 
-    const handlClearCart = () => {
-        cartItems.forEach(item=>{
-            dispatch(removeFromCart((item._id)))
-        })
-    }
+    const handleClearCart = () => {
+        cartItems.forEach(item => {
+            dispatch(removeFromCart(item._id));
+        });
+    };
+
     return (
         <div className="modal">
             <div onClick={toggleModal} className="overlay"></div>
-            <div className="modal-content w-[300px] md:w-[600px] xl:w-[996px] ">
+            <div className="modal-content w-[300px] md:w-[600px] xl:w-[996px]">
                 <h1 className={`${styles.heading} text-center mb-6`}>Кошик</h1>
 
-                <div className={`${styles.menu} flex justify-between block-with-divider text-grey`}>
-                    <div>{amount}</div>
-                    <button onClick={handlClearCart}>Видалити все</button>
-                </div>
-
                 <div className="cart-items">
-                    {cartItems.map((item) => (
-                        <div key={item._id} className="cart-item">
-                            <h2>{item.title}</h2>
-                            <p>Автор: {item.author_id.map(author => `${author.name} ${author.surname}`).join(', ')}</p>
-                            <p>Ціна: ${item.price_id.discounted_price || item.price_id.original_price}</p>
-                        </div>
-                    ))}
+                    {cartItems.length > 0 ? (
+                        <>
+                            <div className={`${styles.menu} cart-heading flex justify-between block-with-divider text-grey`}>
+                                <div>{amount}</div>
+                                <button onClick={handleClearCart}>Видалити все</button>
+                            </div>
+                            {cartItems.map((item) => (
+                                <div key={item._id} className="cart-item">
+                                    <h2>{item.title}</h2>
+                                    <p>Автор: {item.author_id.map(author => `${author.name} ${author.surname}`).join(', ')}</p>
+                                    <p>Ціна: ${item.price_id.discounted_price || item.price_id.original_price}</p>
+                                </div>
+                            ))}
+                        </>
+                    ) : (
+                        <p>Кошик порожній</p>
+                    )}
                 </div>
 
                 <button className="close-modal" onClick={toggleModal}>
