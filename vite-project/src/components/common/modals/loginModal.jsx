a39c482baf23da756cd7e659b2c219be1cdd27b1
Olena Yastrebtsova <uaalena202@gmail.com>
2024-06-17 10:07:16 +0300

    updated: login modal

--- vite-project/src/components/common/modals/loginModal.jsx
+++ vite-project/src/components/common/modals/loginModal.jsx
@@ -1,11 +1,12 @@
-
+import close from '../../../assets/close-modal.svg';
+import styles from '../../../style';
 
 const LoginModal = ({ toggleModal }) => {
     return (
         <div className=" modal">
             <div onClick={toggleModal} className="overlay"></div>
-            <div className=" modal-content">
-                <p>login</p>
+            <div className=" modal-content w-[300px] md:w-[600px] xl:w-[996px] flex flex-col h-[80vh] max-h-[90vh] pb-[50px]">
+            <h1 className={`${styles.heading} text-center mb-6`}>увійти</h1>
                 <button className="close-modal" onClick={toggleModal}>
                     <img src={close} alt="Закрити" />
                 </button>
