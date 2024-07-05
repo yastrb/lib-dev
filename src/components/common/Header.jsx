34fcddf21e56d7b4a6958913860410b1115a4e20
Olena Yastrebtsova <uaalena202@gmail.com>
2024-07-05 15:07:49 +0300

    Fixed: Phone Number

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,40 +1,41 @@
-﻿
+﻿import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import styles from "../../style";
-import logo from "../../assets/logo.svg"
+import logo from "../../assets/logo.svg";
 import SearchBar from './SearchBar';
 import SelectLanguage from './SelectLanguage';
 import Nav from './Nav';
-import { Link } from 'react-router-dom';
 import CartIcon from '../common/Icons/CartIcon';
 import LoginIcon from '../common/Icons/LogInIcon';
 
-
 const Header = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
 
   return (
-    <header className=' w-full '>
+    <header className='w-full'>
       <div className='bg-main pt-[44px]'>
         <div className={`${styles.paddingX} ${styles.boxWidth} mx-auto justify-between flex py-5`}>
+          {isHomePage ? (
+            <img src={logo} alt="BookShop" className='w-[180px] h-[80px]' />
+          ) : (
+            <Link to='/'>
+              <img src={logo} alt="BookShop" className='w-[180px] h-[80px]' />
+            </Link>
+          )}
 
-          <Link to='/'>
-            <img src={logo} alt="BookShop" className=' w-[180px] h-[80px]' />
-          </Link>
-
-          <div className=' hidden md:flex md:flex-col md:items-center md:justify-center relative'>
+          <div className='hidden md:flex md:flex-col md:items-center md:justify-center relative'>
             <SearchBar />
           </div>
 
-          <div className=' flex md:items-center md:justify-center'>
-
-            <div className=" hidden md:flex  h-12">
+          <div className='flex md:items-center md:justify-center'>
+            <div className="hidden md:flex h-12">
               <SelectLanguage />
             </div>
-
             <div className="flex items-center justify-center">
-              <CartIcon/>
-              <LoginIcon/>
+              <CartIcon />
+              <LoginIcon />
             </div>
-
           </div>
         </div>
       </div>
@@ -42,9 +43,8 @@ const Header = () => {
       <div className="bg-secondary">
         <Nav />
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
