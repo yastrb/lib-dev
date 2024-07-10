2a45756ac48028d565dadae3031524bbc0cd29a9
Olena Yastrebtsova <uaalena202@gmail.com>
2024-07-10 17:53:27 +0300

    react i18next added

---
+++ src/utils/i18n.js
@@ -0,0 +1,31 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// the translations
+// (tip move them in a JSON file and import them,
+// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+const resources = {
+  en: {
+    translation: {
+      "Welcome to React": "Welcome to React and react-i18next"
+    }
+  },
+  fr: {
+    translation: {
+      "Welcome to React": "Bienvenue à React et react-i18next"
+    }
+  }
+};
+
+i18n
+  .use(initReactI18next) 
+  .init({
+    resources,
+    lng: "en", 
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18n;
