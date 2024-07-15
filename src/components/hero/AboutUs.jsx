db7ab7590b124d91412e2cebb9f089c4aa197df0
Olena Yastrebtsova <uaalena202@gmail.com>
2024-07-15 16:55:48 +0300

    Fixed: Locales

--- src/components/hero/AboutUs.jsx
+++ src/components/hero/AboutUs.jsx
@@ -1,20 +1,24 @@
 ﻿import styles from '../../style';
+import { Suspense } from 'react';
 import bgImage from "../../assets/bgImage.jpg";
 import { useTranslation } from 'react-i18next';
 
 function AboutUsText() {
-  const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
 
-  return (
-    <div className='relative w-full h-[235px] md:h-[262px] lg:h-[348px]'>
-      <img src={bgImage} alt="bg-image" className='w-full h-full object-cover' />
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <article className={`${styles.boxWidth} ${styles.paddingX} ${styles.paddingY} text-[12px] md:text-base lg:text-2xl text-center leading-[1.6]`}>
-          {t('welcome')}
-        </article>
-      </div>
-    </div>
-  );
+    return (
+        <Suspense fallback="loading">
+            <div className='relative w-full h-[235px] md:h-[262px] lg:h-[348px]'>
+                <img src={bgImage} alt="bg-image" className='w-full h-full object-cover' />
+                <div className='absolute inset-0 flex items-center justify-center'>
+                    <article className={`${styles.boxWidth} ${styles.paddingX} ${styles.paddingY} text-[12px] md:text-base lg:text-2xl text-center leading-[1.6]`}>
+                        {t('main.hero')}
+                    </article>
+                </div>
+            </div>
+        </Suspense>
+
+    );
 }
 
 export default AboutUsText;
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,23 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 
 i18n
+  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: {
-          "welcome": "Welcome to the book store",
-        }
-      },
-      uk: {
-        translation: {
-          "welcome": "Ласкаво просимо до книжкового магазину",
-        }
-      }
-    },
-    lng: "uk",
+    // resources: {
+    //   en: {
+    //     translation: {
+    //       "welcome": "Welcome to the book store",
+    //     }
+    //   },
+    //   uk: {
+    //     translation: {
+    //       "welcome": "Ласкаво просимо до книжкового магазину",
+    //     }
+    //   }
+    // },
+    // lng: "uk",
+    debug: true,
     fallbackLng: "uk",
 
     interpolation: {
