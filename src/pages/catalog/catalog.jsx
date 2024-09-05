90aa7be615bc96a366b2284b8090eec87066fb67
Olena Yastrebtsova <uaalena202@gmail.com>
2024-09-05 14:08:57 +0300

    sorting fixed

--- src/pages/catalog/catalog.jsx
+++ src/pages/catalog/catalog.jsx
@@ -5,13 +5,18 @@ import FiltersMenu from '../../components/filtersMenu/index.jsx'
 import styles from '../../style.js'
 import './styles.css'
 import Cataloglist from './cataloglist.jsx'
+import { useGetNewBestsellersSalesBooks } from '../../redux/booksSlice'
 
 const Catalog = () => {
+
 	const [menuStatus, setMenuStatus] = useState(false)
+	const { data, error, isLoading } = useGetNewBestsellersSalesBooks();
+    const [sortedBooks, setSortedBooks] = useState([]);
 
 	const handleChangeMenuStatus = () => {
 		setMenuStatus(prevStatus => !prevStatus)
 	}
+
 	useEffect(() => {
 		if (menuStatus) {
 			document.body.style.overflow = 'hidden'
@@ -22,6 +27,9 @@ const Catalog = () => {
 			document.body.style.overflow = ''
 		}
 	}, [menuStatus])
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error loading books</div>;
 	return (
 		<div className={`${styles.boxWidth} m-auto  min-h-screen catalogContainer`}>
 			<FiltersMenu menuStatus='true' desctopOnly='true' />
@@ -29,7 +37,7 @@ const Catalog = () => {
 			<div className='catalogContent__wrapper'>
 				<div className='flex justify-between w-full'>
 					<p className='catalogTotalAmountItems'>305 товарів</p>
-					<DropDownCatalogSort />
+					<DropDownCatalogSort books={data.newBooks} onSort={setSortedBooks} />
 				</div>
 
 				{/* ----MobileVersion---- */}
@@ -41,10 +49,8 @@ const Catalog = () => {
 					changeMenuStatus={handleChangeMenuStatus}
 					bgRender='true'
 				/>
-			
 				{/* -------------------- */}
-
-				<Cataloglist/>
+				<Cataloglist books={sortedBooks.length > 0 ? sortedBooks : data.newBooks}/>
 			</div>
 		</div>
 	)
--- src/pages/catalog/cataloglist.jsx
+++ src/pages/catalog/cataloglist.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react';
-import { useGetNewBestsellersSalesBooks } from '../../redux/booksSlice'
-import BookCard from '../../components/product/BookCard'
-import DropDownCatalogSort from '../../components/dropDownCatalogSort';
+import BookCard from '../../components/product/BookCard';
 
-const Cataloglist = () => {
-    const { data, error, isLoading } = useGetNewBestsellersSalesBooks();
-    const [sortedBooks, setSortedBooks] = useState([]);
-
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error loading books</div>;
-
-
-    // не працює поки дублюються книги
-    // const allBooks = [
-    //     ...(data.newBooks || []),
-    //     ...(data.salesBooks || []),
-    //     ...(data.bestsellerBooks || [])
-    // ];
-
+const Cataloglist = ({ books }) => {
     return (
         <section className='my-4'>
-            <DropDownCatalogSort books={data.newBooks} onSort={setSortedBooks} />
-            <ul className='grid grid-cols-1 gap-4'>
-                {(sortedBooks.length > 0 ? sortedBooks : data.newBooks).map(book => (
+            <ul className='grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center'>
+                  {books.map(book => (
                     <BookCard key={book._id} book={book} />
                 ))}
+             
             </ul>
         </section>
-    )
-}
+    );
+};
 
 export default Cataloglist;
