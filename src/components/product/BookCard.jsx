7befc9449caa140a250a07d3c1fcab42130efd2f
Olena Yastrebtsova <uaalena202@gmail.com>
2024-10-04 20:38:13 +0300

    cart fixed

--- src/components/product/BookCard.jsx
+++ src/components/product/BookCard.jsx
@@ -1,20 +1,22 @@
-// import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-// import { addToCart } from '../../redux/cartSlice'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cartSlice';
+import { useNavigate } from 'react-router-dom';
+
 const BookCard = ({ book }) => {
-	// const dispatch = useDispatch()
-	// const handleAddToCart = () => {
-	// 	dispatch(addToCart(book))
-	// }
-	const navigate = useNavigate()
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleAddToCart = () => {
+	  dispatch(addToCart(book));
+	};
 
 	const handleClick = () => {
-		navigate('/productpage', { state: { id: book.price[0].book_id } })
-		window.scroll(0, 0)
-	}
+		navigate('/productpage', { state: { id: book.price[0].book_id } });
+		window.scroll(0, 0);
+	};
 
 	return (
-		<div className='border-solid w-44'>
+		<div className='book-card mx-8 border-solid w-44'>
 			<img
 				className='rounded-xl w-44 h-60 cursor-pointer'
 				src={book.coverImageLink_ukr}
@@ -22,28 +24,28 @@ const BookCard = ({ book }) => {
 				onClick={handleClick}
 			/>
 
-			<h2 className=' ml-1 font-medium font-montserrat my-4 w-44 truncate'>
+			<h2 className='font-medium font-montserrat my-4 w-44 truncate'>
 				{book.title_ukr}
 			</h2>
-			<p className='font-montserrat ml-1 truncate'>
+			<p className='font-montserrat truncate'>
 				{book.author
 					.map(author => `${author.name_ukr} ${author.surname_ukr}`)
 					.join(', ')}
 			</p>
-			<p className='font-montserrat ml-1 my-4 font-medium '>
+			<p className='font-montserrat my-4 font-medium'>
 				{book.price[0].discounted_price > 0
 					? `${book.price[0].discounted_price} грн`
 					: `${book.price[0].original_price} грн`}
 			</p>
 
 			<button
-				className=' bg-button rounded-lg w-full py-2 hover:bg-hover font-montserrat duration-300'
-				// onClick={handleAddToCart}
+				className='bg-button rounded-lg p-2 hover:bg-hover font-montserrat duration-300'
+				onClick={handleAddToCart}
 			>
-				В кошик
+				Add to Cart
 			</button>
 		</div>
-	)
+	);
 }
 
-export default BookCard
+export default BookCard;
