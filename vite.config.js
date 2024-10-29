f1bdce0f8aa19179e88bd253da6a3130ac72d064
Olena Yastrebtsova <uaalena202@gmail.com>
2024-10-29 18:32:47 +0200

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +10,10 @@ export default defineConfig({
       external: ['axios'], 
     },
   },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components')
+    }
+  }
 })
 
