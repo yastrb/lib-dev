e28a921803585a7a67a7937e5587d2eacc7eaee4
Olena Yastrebtsova <78073513+yastrb@users.noreply.github.com>
2024-11-19 17:35:50 +0200

    Delete src/sections/headerSection directory

--- src/sections/headerSection/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HeaderSection} from './Header'
-export { default as Navbar } from './Navbar/Navbar';
-export { default as DesktopNav } from './DesktopNav/DesktopNav';
-export { default as MobileNav } from './MobileNav/MobileNav';
