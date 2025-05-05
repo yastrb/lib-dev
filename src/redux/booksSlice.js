30b7b28ab7d47040b578fe9d076f1d78a1f3f692
Olena Yastrebtsova <uaalena202@gmail.com>
2025-05-05 19:59:33 +0300

    refactor:updated catalog.jsx

--- src/redux/booksSlice.js
+++ src/redux/booksSlice.js
@@ -6,9 +6,8 @@ export const booksDataApi = createApi({
     baseUrl: "https://biblioteka-backend-btd3.onrender.com",
   }),
   endpoints: (builder) => ({
-
-    getNewBooks: builder.query({
-      query: () => "/new",
+    getAllBooks: builder.query({
+      query: () => "/books",
       transformResponse: (response) =>
         response.map((book) => ({
           ...book,
@@ -18,33 +17,46 @@ export const booksDataApi = createApi({
           author: book.author || "Unknown Author",
           summary: book.description || "No summary available",
         })),
-    }),
 
-    getBestsellers: builder.query({
-      query: () => "/bestseller",
+      getNewBooks: builder.query({
+        query: () => "/new",
+        transformResponse: (response) =>
+          response.map((book) => ({
+            ...book,
+            title: book.title || "Unknown Title",
+            coverImageLink: book.images?.length > 0 ? [book.images[0].url] : [],
+            price: book.price || 0,
+            author: book.author || "Unknown Author",
+            summary: book.description || "No summary available",
+          })),
+      }),
 
-      transformResponse: (response) =>
-        response.map((book) => ({
-          ...book,
-          title: book.title || "Unknown Title",
-          coverImageLink: book.images?.length > 0 ? [book.images[0].url] : [],
-          price: book.price || 0,
-          author: book.author || "Unknown Author",
-          summary: book.description || "No summary available",
-        })),
-    }),
+      getBestsellers: builder.query({
+        query: () => "/bestseller",
 
-    getPromotionBooks: builder.query({
-      query: () => "/promotion",
-      transformResponse: (response) =>
-        response.map((book) => ({
-          ...book,
-          title: book.title || "Unknown Title",
-          coverImageLink: book.images?.length > 0 ? [book.images[0].url] : [],
-          price: book.price || 0,
-          author: book.author || "Unknown Author",
-          summary: book.description || "No summary available",
-        })),
+        transformResponse: (response) =>
+          response.map((book) => ({
+            ...book,
+            title: book.title || "Unknown Title",
+            coverImageLink: book.images?.length > 0 ? [book.images[0].url] : [],
+            price: book.price || 0,
+            author: book.author || "Unknown Author",
+            summary: book.description || "No summary available",
+          })),
+      }),
+
+      getPromotionBooks: builder.query({
+        query: () => "/promotion",
+        transformResponse: (response) =>
+          response.map((book) => ({
+            ...book,
+            title: book.title || "Unknown Title",
+            coverImageLink: book.images?.length > 0 ? [book.images[0].url] : [],
+            price: book.price || 0,
+            author: book.author || "Unknown Author",
+            summary: book.description || "No summary available",
+          })),
+      }),
     }),
   }),
 });
@@ -53,4 +65,5 @@ export const {
   useGetNewBooksQuery,
   useGetBestsellersQuery,
   useGetPromotionBooksQuery,
+  useGetAllBooksQuery,
 } = booksDataApi;
