9faeb1e5b0566ffb11a9b7f7b82e8ac236fdc994
Olena Yastrebtsova <uaalena202@gmail.com>
2025-05-07 11:16:56 +0300

    refactor: getBooks.js

--- src/ui/SearchBar/api/getBooks.js
+++ src/ui/SearchBar/api/getBooks.js
@@ -1,10 +1,26 @@
 import axios from '/node_modules/axios';
 
-const getBooks = async () => {
-	const response = await axios.get(
-		'https://biblioteka-backend-btd3.onrender.com/api/books'
-	);
-	return response.data.content;
+const getBooks = async (content = {}) => {
+  try {
+    const response = await axios.get(
+      'https://biblioteka-backend-btd3.onrender.com/api/books/search',
+      {
+        params: {
+          title: content.title || '',   
+          author: content.author || '', 
+          category: content.category || '', 
+          year: content.year || '',     
+          language: content.language || '' 
+        },
+      }
+    );
+	console.log("search books",response.data.content);
+    return response.data.content; 
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    return []; 
+  }
 };
 
 export default getBooks;
+
